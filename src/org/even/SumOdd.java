ae1451a595ca9525d1470d23634a5795c6daa33a
gurugo12 <gobiit012@gmail.com>
2018-12-29 22:18:00 +0530

    Sample prgm

---
+++ src/org/even/SumOdd.java
@@ -0,0 +1,22 @@
+package org.even;
+
+public class SumOdd 
+{
+	
+	public static void main(String[] args) 
+	{
+		int a=0;
+		
+		for(int i=1;i<=100;i++)
+		{
+	    if(i%2!=0)
+	    {
+		a=a+i;	
+	    }
+	    
+		}
+
+		System.out.println("Sum of the ODD number is "+a);
+	}
+
+}
